ef540796ed0bd2e88166da631cc506aa8039bcfc
AmirAli <aafz1382@gmail.com>
2023-07-04 12:11:05 +0330

    updated

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,4 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -8,5 +6,30 @@ var swiper = new Swiper(".mySwiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
-  }
+  },
+
+  // Default parameters   
+  slidesPerView: 4,   
+  spaceBetween: 40,   
+  // Responsive breakpoints   
+  breakpoints: {  
+  
+     // when window width is <= 320px     
+     480: {       
+        slidesPerView: 1,
+        spaceBetween: 10     
+     },     
+     // when window width is <= 480px     
+     640: {       
+        slidesPerView: 2,       
+        spaceBetween: 20     
+     },   
+ 
+     // when window width is <= 640px     
+     720: {       
+        slidesPerView: 3,       
+        spaceBetween: 30     
+     } 
+ 
+  } 
 });
